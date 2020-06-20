aaa528ccc8a0e0f6dd09dadf6af987057cf9e150
Theresa Yip <ymy.theresa@gmail.com>
2020-06-20 17:22:58 +0800

    Libraries

--- LibraryExample.sol
+++ LibraryExample.sol
@@ -1,8 +1,27 @@
 pragma solidity ^0.6.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol";
+library Search {
+    function indexOf(uint[] storage self, uint value) public view returns(uint){
+        for(uint i = 0; i < self.length; i++){
+            if (self[i] == value) return i;
+            return MAX(uint(256));
+        }
+    }
+}
 
-contract LibraryExample{
-    using SafeMath for uint;
-    mapping (address => uint)
+contract UsingExample{
+    using Search for uint[];
+    uint[] data;
+
+    function usingIndex(uint _value) public view returns(uint){
+        return data.indexOf(_value);
+    }
+}
+
+contract NotUsingExample{
+    uint[] data;
+
+    function notUsingIndex(uint _value) public view returns(uint){
+        return Search.indexOf(data, _value);
+    }
 }
